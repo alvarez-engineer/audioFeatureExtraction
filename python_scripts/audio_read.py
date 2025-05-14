441ce84529165496ca213a93b3188ba8552b88d6
smcclintock <githubsmc@gmail.com>
2025-05-14 11:31:46 -0700

    update audio_read.py

--- python_scripts/audio_read.py
+++ python_scripts/audio_read.py
@@ -10,8 +10,11 @@ CHUNK = 1024
 FILE_LOCATION = '../data/'
 FILE_NAME = 'file_example_WAV_1MG.wav'
 FILE_PATH = FILE_LOCATION + FILE_NAME
-print(f'Attempting to play {FILE_PATH}.')
 
+# The 'f'before the string is shorthand for 'str.format()' and allows for embed expressions to strings
+# '\n' prints new line
+print(f"\nAttempting to play {FILE_PATH}.")
+print("The ALSA will be unable to find some audio drivers, that is fine for now.\n")
 
 with wave.open(FILE_PATH, 'rb') as wf:
     # Instantiate PyAudio and initialize PortAudio system resources
@@ -26,6 +29,7 @@ with wave.open(FILE_PATH, 'rb') as wf:
         )
 
     # Play samples from the wave file
+    # Documentation on the Assignment Expression ':=' linked in google doc
     while len(data := wf.readframes(CHUNK)):
         stream.write(data)
 
@@ -34,5 +38,3 @@ with wave.open(FILE_PATH, 'rb') as wf:
 
     # Release PortAudio system resources
     p.terminate()
-
-print("The ALSA will be unable to find some audio drivers, that is fine for now")
